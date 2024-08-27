9e96ee209353d8ec628aa5a3119da526cfb033b6
khaled200588 <155583427+khaled200588@users.noreply.github.com>
2024-08-27 13:18:05 -0700

    Add files via upload

---
+++ E_Matrix.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define NoSt0n_on_da_code std::ios::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+
+typedef long long ll;
+
+#define MP make_pair
+#define PB push_back
+#define EmB emplace_back
+#define Em emplace
+#define len(s) (ll) s.length()
+#define sz(v) (ll) v.size()
+#define all(a) a.begin(), a.end()
+#define all_r(a) a.rbegin(), a.rend()
+#define mem(x, val) memset((x), (val), sizeof(x))
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+
+#define tst     \
+    ll t;       \
+    cin >> t;   \
+    ll i_t = 0; \
+    while (i_t++ < t)
+#define SetPre(n, x) cout << fixed << setprecision(n) << x
+
+#define int long long
+
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define F0R(i, a) for (int i = 0; i < a; i++)
+#define F1R(i, a) for (int i = 1; i <= a; i++)
+#define cin(v)        \
+    for (auto &i : v) \
+        cin >> i;
+        
+#define cout(v)      \
+    for (auto i : v) \
+        cout << i << " ";
+#define el "\n";
+
+
+
+
+
+
+void Do_it(int TC){
+  int r , c;
+    cin >> r >> c;
+    int matrix [r] [c];
+    F0R (i , r) {
+        F0R (j , c) {
+            cin >> matrix [i] [j];
+        }
+    }
+
+    F0R (i , r) {
+        F0R (j , c) {
+            int mx = 0;
+            bool flag = true;
+            if ( i - 1 >= 0 ) {
+                mx = max (mx , matrix [i - 1] [j]);
+            }
+            if ( j - 1 >= 0 ) {
+                mx = max (mx , matrix [i] [j - 1]);
+            }
+            if ( j + 1 < c ) {
+                mx = max (mx , matrix [i] [j + 1]);
+            }
+            if ( i + 1 < r ) {
+                mx = max (mx , matrix [i + 1] [j]);
+            }
+            if ( i - 1 >= 0 && matrix [i] [j] <= matrix [i - 1] [j] ) {
+                flag = false;
+            }
+            if ( j - 1 >= 0 && matrix [i] [j] <= matrix [i] [j - 1] ) {
+                flag = false;
+            }
+            if ( j + 1 < c && matrix [i] [j] <= matrix [i] [j + 1] ) {
+                flag = false;
+            }
+            if ( i + 1 < r && matrix [i] [j] <= matrix [i + 1] [j] ) {
+                flag = false;
+            }
+            if ( flag ) {
+                matrix [i] [j] = mx;
+            }
+        }
+    }
+
+    F0R (i , r) {
+        F0R (j , c) {
+            cout << matrix [i] [j] << " ";
+        }
+    }
+}
+
+signed main(){
+    NoSt0n_on_da_code int _ = 1;
+    cin >> _;
+    for (int __ = 1; __ <= _; __++){
+        // cout << "Case #" << __ << " : ";
+        Do_it(__);
+        if (__ < _){
+            cout << '\n';
+            // cout << '\n';
+        }
+    }
+    return 0;
+}
+// I Can Do It
+// Fight not to be the Winner,but to be the last one to lose
